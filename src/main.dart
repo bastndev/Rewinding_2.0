bc855fd9d769d4b8569e59d753b7d81a3cada31a
bastndev <gohit24@gmail.com>
2023-07-13 11:15:51 -0500

    :alien: Update `First Class`

--- src/main.dart
+++ src/main.dart
@@ -1,4 +1,40 @@
-void main (){
+/* void main (){
 
   print("hello World");
+} */
+// FIXME: Practice (JAVA)
+
+/* int sum = 0;
+int result = 0;
+
+for(int val : array){
+  sum += val;
 }
+
+for (int x : array ){
+  for (int y : array){
+    result += x * y - sum;
+  }
+}
+ */
+// TODO:: Practice (DART)
+void main() {
+  int sum = 0;
+  int result = 0;
+
+  List<int> array = [1,2,3,-4];
+
+  for (int val in array) {
+    sum += val;
+  }
+
+  for (int x in array) {
+    for (int y in array) {
+      result += x * y - sum;
+    }
+  }
+
+  print(result);
+}
+
+// TODO:Complexity
