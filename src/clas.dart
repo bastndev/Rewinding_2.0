ec4f980a2ef894259ac061cb35ce3e8f5d56def1
bastndev <gohit24@gmail.com>
2023-08-10 13:10:48 -0500

    :mango: Update `Class 7.0`

--- src/clas.dart
+++ src/clas.dart
@@ -13,7 +13,7 @@ void main() {
   print("The sum of ${sum1.num1} and ${sum1.num2} is: $result");
 } */
 
-// FIXME: Update Class | Practice 
+// FIXME: Update Class | Practice
 
 /* class MultiNumber{
   double num1;
@@ -110,7 +110,7 @@ void main (){
 } */
 
 // FIXME: Class
-class Hero{
+/* class Hero{
   String? power;
   String? name;
 
@@ -129,6 +129,31 @@ void main (){
   final rawJson = {
     'name' : 'Tony Stark',
     'power': 'Money',
+  };
+  final ironman = Hero.fromJson(rawJson);
+  print(ironman);
+} */
+
+//FIXME: Class
+class Hero {
+  String name;
+  String power;
+
+  Hero({required this.name, required this.power});
+
+  Hero.fromJson(Map<String, String> json):
+    this.name = json['name']!,
+    this.power = json['power']??"He don't have Power";
+
+  String toString() {
+    return 'Hero:\nname:${this.name}\nPower:${this.power}';
+  }
+}
+
+void main() {
+  final rawJson = {
+    'name': 'Tony Stark',
+    // 'power': 'Money',
   };
   final ironman = Hero.fromJson(rawJson);
   print(ironman);
