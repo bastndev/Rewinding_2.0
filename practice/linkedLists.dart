38dec17e2c51c1c7695d459d46c69a3fa138d2ee
bastndev <gohit24@gmail.com>
2023-08-12 12:12:21 -0500

    :apple: Update `Readme.md`

--- practice/linkedLists.dart
+++ practice/linkedLists.dart
@@ -1,5 +1,5 @@
 //  TODO: Delete elements or number O(N)
-class Node {
+/* class Node {
   dynamic data;
   Node? next;
 
@@ -65,6 +65,119 @@ void main() {
 
   print("\nList after removing duplicates:");
   linkedList.display();
-}
+} */
 
 //FIXME: Practice LinkedList | Delete NUmber
+/* class Node {
+  dynamic data;
+  Node? next;
+
+  Node(this.data);
+}
+
+class LinkedList {
+  Node? head;
+
+  void insert(dynamic data) {
+    var newNode = Node(data);
+    head == null ? head = newNode : tailNode().next = newNode;
+  }
+
+  Node tailNode() {
+    var current = head;
+    while (current!.next != null) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  void removeDuplicates() {
+    var current = head;
+    while (current != null) {
+      var runner = current;
+      while (runner.next != null) {
+        if (runner.next!.data == current.data) {
+          runner.next = runner.next!.next;
+        } else {
+          runner = runner.next!;
+        }
+      }
+      current = current.next;
+    }
+  }
+
+  void display() {
+    var current = head;
+    while (current != null) {
+      print(current.data);
+      current = current.next;
+    }
+  }
+}
+
+void main() {
+  var inputList = [1, 2, 2, 3, 4, 1];
+  var linkedList = LinkedList();
+
+  inputList.forEach((item) => linkedList.insert(item));
+
+  print("Original List:");
+  linkedList.display();
+
+  linkedList.removeDuplicates();
+
+  print("\nList after removing duplicates:");
+  linkedList.display();
+}
+ */
+
+// FIXME: practice 2 
+class Node {
+  dynamic data;
+  Node? next;
+  Node(this.data);
+}
+
+class LinkedList {
+  Node? head;
+  void insert(dynamic data) {
+    var newNode = Node(data);
+    head == null ? head = newNode : tailNode().next = newNode;
+  }
+  Node tailNode() => head == null ? head! : _tailNode(head!);
+
+  Node _tailNode(Node node) => node.next == null ? node : _tailNode(node.next!);
+
+  void removeDuplicates() {
+    var current = head;
+    while (current != null) {
+      var runner = current;
+      while (runner.next != null) {
+        if (runner.next!.data == current.data) {
+          runner.next = runner.next!.next;
+        } else {
+          runner = runner.next!;
+        }
+      }
+      current = current.next;
+    }
+  }
+  void display() {
+    var current = head;
+    while (current != null) {
+      print(current.data);
+      current = current.next;
+    }
+  }
+}
+
+void main() {
+  var inputList = [1, 2, 2, 3, 4, 1];
+  var linkedList = LinkedList();
+  inputList.forEach((item) => linkedList.insert(item));
+  print("Original List:");
+  linkedList.display();
+  linkedList.removeDuplicates();
+  print("\nList after removing duplicates:");
+  linkedList.display();
+}
