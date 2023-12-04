053b1ff11c49606aa43d5a4e57250005fb043af1
bastndev <gohit24@gmail.com>
2023-12-04 16:59:12 -0500

    :arrow_up: Update `new`

--- LOGIC/Bliclues_arrays_matrizes.dart
+++ LOGIC/Bliclues_arrays_matrizes.dart
@@ -1,9 +1,40 @@
 // --- -TODO: Loop while
 
+/* void main() {
+  int x = 5;
+
+  while (x > 0) {
+    x * 2;
+  }
+  print("aea mano $x");
+}
+ */
+
+/* void main() {
+  int x = 5;
+
+  while (x < 10) {
+    x = x * 2;
+  }
+  print("El valor final de x es: $x");
+} */
+
+
+
+/* void main() {
+  int x = 5;
+
+  do {
+    x = x * 2;
+  } while (x < 10);
+  print("El valor final de x es: $x");
+}
+ */
+
 void main() {
   int x = 5;
 
-  while (x < 0) {
+  while (x > 0) {
     x * 2;
   }
   print("aea mano $x");
