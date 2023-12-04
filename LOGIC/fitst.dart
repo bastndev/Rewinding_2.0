9e1947e16b0cece02697264ae764ab9a71101632
bastndev <gohit24@gmail.com>
2023-12-04 09:44:00 -0500

    :arrow_up: Update `new`

---
+++ LOGIC/fitst.dart
@@ -0,0 +1,28 @@
+//--- --- -TODO: variables, SWAP VALOR WHIT TIME
+void main() {
+  // Numeric variables
+  int age = 25;
+  double height = 1.75;
+
+  // Text variables
+  String name = 'John';
+
+  // Boolean variable
+  bool isStudent = true;
+
+  // List (Array)
+  List<String> colors = ['red', 'green', 'blue'];
+
+  // Map (Dictionary)
+  Map<String, dynamic> person = {
+    'name': 'Anna',
+    'age': 30,
+    'student': false,
+  };
+
+  // Print values
+  print('Name: $name, Age: $age, Height: $height');
+  print('Is student: $isStudent');
+  print('Colors: $colors');
+  print('Person information: $person');
+}
