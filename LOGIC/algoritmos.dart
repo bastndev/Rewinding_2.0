dea15de09981ada1d158c8771400c885f8e27287
bastndev <gohit24@gmail.com>
2023-12-05 09:25:19 -0500

    :arrow_up: Update `new`

--- LOGIC/algoritmos.dart
+++ LOGIC/algoritmos.dart
@@ -1,12 +1,21 @@
 // --- --- -TODO: Circle area
-void main() {
+/* void main() {
   int radio = 2;
   double pi = 3.14;
 
   double area = radio * radio * pi;
   print("The area circle is: $area");
-}
+} */
 
 // --- --- -TODO: Rectangle area
+
+void main() {
+  double a = 13;
+  double b = 3;
+
+  double area = a * b;
+
+  print("The area rectangle is: $area");
+}
 
 // --- --- -TODO: Square area
