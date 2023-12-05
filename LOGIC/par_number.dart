0cde7aed908a2f4214e41e0e9d174f764d9cf841
bastndev <gohit24@gmail.com>
2023-12-05 10:05:09 -0500

    :arrow_up: Update `new`

---
+++ LOGIC/par_number.dart
@@ -0,0 +1,12 @@
+void main() {
+  int number = 21;
+
+  if (number == 0) {
+    print("Enter a number");
+  } else if (number % 2 == 0) {
+    print("The number: $number matched");
+  } else {
+    print("the number is unmatched");
+  }
+}
+
