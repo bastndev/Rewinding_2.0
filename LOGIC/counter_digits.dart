34e92482c8f03b4709546ec582a948376ef1c728
bastndev <gohit24@gmail.com>
2023-12-05 10:48:27 -0500

    :arrow_up: Update `new`

--- LOGIC/counter_digits.dart
+++ LOGIC/counter_digits.dart
@@ -1,4 +1,4 @@
-void main() {
+/* void main() {
   int num = 99999999;
 
   if (num > 0 && num <= 9999) {
@@ -17,4 +17,28 @@ void main() {
   } else {
     print("Wtf pls enter number, but inly 4 digits, Thanks!");
   }
+} */
+
+void printNumberOfDigits(int num) {
+  if (num < 0 || num > 9999) {
+    print("Wtf pls enter number, but only 4 digits, Thanks!");
+    return;
+  }
+
+  if (num < 10) {
+    print("The number has 1 digit");
+  } else if (num < 100) {
+    print("The number has 2 digits");
+  } else if (num < 1000) {
+    print("The number has 3 digits");
+  } else {
+    print("The number has 4 digits");
+  }
 }
+
+void main() {
+  int num = 99; // You may want to get the input from the user
+
+  printNumberOfDigits(num);
+}
+
