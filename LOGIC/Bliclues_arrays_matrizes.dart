cb8c109c4e3484d50ff55b81c967645076f5e819
bastndev <gohit24@gmail.com>
2023-12-04 20:27:48 -0500

    :arrow_up: Update `new`

--- LOGIC/Bliclues_arrays_matrizes.dart
+++ LOGIC/Bliclues_arrays_matrizes.dart
@@ -48,3 +48,45 @@
   print(x);
 }
  */
+
+/* void main() {
+  int x = 10;
+
+  if (x != 10) {
+    print("Error: La condición inicial ya es falsa, el bucle no se ejecutó.");
+  } else {
+    while (x != 10) {
+      x += 10;
+    }
+    print(x);
+  }
+}
+ */
+/* void main() {
+  int x = 5;
+
+  while (x > 0) {
+    x * 2;
+  }
+  print("The result is $x");
+}
+ */
+/* void main() {
+  int x = 5;
+
+  while (x < 10) {
+    x = x * 2;
+  }
+  print("El valor final de x es: $x");
+} */
+
+void main() {
+  int x = 2;
+
+  if (x > 4) {
+    x = x * 2;
+    print("Si se multiple $x");
+  } else {
+    print("No se multiple $x");
+  }
+}
