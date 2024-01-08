17d41914f7e0526d87d4a967fbbd04890c26f70c
bastndev <gohit24@gmail.com>
2024-01-08 10:33:01 -0500

    :arrow_up: Update `RECENT`

--- 04-LINKED LISTS/cascade_operator.dart
+++ 04-LINKED LISTS/cascade_operator.dart
@@ -1,5 +1,5 @@
 //Before:
-void main() {
+/* void main() {
   List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   list.clear();
@@ -7,6 +7,19 @@ void main() {
   list.addAll([11, 12, 13]);
   list.removeAt(1);
   list.insert(3, 35);
+
+  print("Traditional list: $list");
+} */
+
+void main() {
+  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  
+  list
+    ..clear()
+    ..add(10)
+    ..addAll([11, 12, 13])
+    ..removeAt(1)
+    ..insert(3, 35);
 
   print("Traditional list: $list");
 }
