6041047d95751ec142f19f549e6cca7987d1d62f
bastndev <gohit24@gmail.com>
2024-01-08 09:50:32 -0500

    :arrow_up: Update `RECENT`

--- 04-LINKED LISTS/exercise.dart
+++ 04-LINKED LISTS/exercise.dart
@@ -1,7 +1,17 @@
 // Example
-// Input [1,,2,2,3,4,1]
+// Input [1,2,2,3,4,1]
 // Output [1,2,3,4]
 
-void main() { 
-  
+void main() {
+  final List<int> list = [1, 2, 2, 3, 4, 1];
+
+  List<int> unList = [];
+
+  for (int i = 0; i < list.length; i++) {
+    if (!unList.contains(list[i])) {
+      unList.add(list[i]);
+    }
+  }
+
+  print("The new numbers are: $unList");
 }
