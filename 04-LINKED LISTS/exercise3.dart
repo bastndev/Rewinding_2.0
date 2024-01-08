2b9b6812687efe05166250a82349f4930eefce45
bastndev <gohit24@gmail.com>
2024-01-08 11:08:45 -0500

    :arrow_up: Update `RECENT`

--- 04-LINKED LISTS/exercise3.dart
+++ 04-LINKED LISTS/exercise3.dart
@@ -16,10 +16,24 @@
 }
  */
 
-void main() {
+/* void main() {
   final List<int> list = [1, 2, 4, 6, 8, 10];
 
   final newNumber = list[list.length - 2];
 
   print("The number is: $newNumber");
+}
+ */
+
+void main() {
+  final List<int> list = [1, 2, 4, 6, 8, 10, 12];
+  final n = 2;
+
+  if (n > 0 && n <= list.length) {
+    final newNumber = list[list.length - n - 0];
+    print("The second to last number is: $newNumber");
+  } else {
+    print(
+        "The value $n does not correspond to a second-to-last element in the list.");
+  }
 }
