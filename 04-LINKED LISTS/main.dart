c24cc7313b4944c3bb3aa7ab07cc0ad300093ad3
bastndev <gohit24@gmail.com>
2024-01-08 09:26:15 -0500

    :arrow_up: Update `RECENT`

--- 04-LINKED LISTS/main.dart
+++ 04-LINKED LISTS/main.dart
@@ -1,7 +1,21 @@
-void main() {
+/* void main() {
   List<int> numbers = [4, 3, 2, 1];
 
   numbers.remove(2);
 
   print(numbers);
+}
+ */
+
+// -TODO: LOOP, while
+void main() {
+  final List<int> numbers = [4, 3, 2, 1];
+
+  print('Original list: $numbers');
+
+  while (numbers.contains(2)) {
+    numbers.remove(2);
+  }
+
+  print('List after Delete 2: $numbers');
 }
