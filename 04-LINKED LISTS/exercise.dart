e4290044d0f0d293ab3c0761d0c05865da09a336
bastndev <gohit24@gmail.com>
2024-01-08 09:59:17 -0500

    :arrow_up: Update `RECENT`

--- 04-LINKED LISTS/exercise.dart
+++ 04-LINKED LISTS/exercise.dart
@@ -16,10 +16,25 @@
   print("The new numbers are: $unList");
 } */
 
-void main() {
+/* void main() {
   final List<int> list = [1, 2, 2, 3, 4, 1, 5, 5, 5, 5, 5, 5];
 
   List<int> unList = list.toSet().toList();
 
   print("The new numbers are: $unList");
+} */
+
+void main() {
+  final List<String> names = [
+    'Lucas',
+    'Juca',
+    'Pedro',
+    'Lucas',
+    'Maria',
+    'Maria'
+  ];
+
+  List<String> unList = names.toSet().toList();
+
+  print("Hello: $unList");
 }
