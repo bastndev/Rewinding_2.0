a0ab50b5f38186129ca66a89454c3651066b8199
bastndev <gohit24@gmail.com>
2024-01-08 10:14:08 -0500

    :arrow_up: Update `RECENT`

--- 04-LINKED LISTS/exercise2.dart
+++ 04-LINKED LISTS/exercise2.dart
@@ -2,7 +2,8 @@
 /*Input: [1,2,4,6], [2,3,5] 
   Output: [1,2,2,3,4,5,6] 
  */
-void main() {
+
+/* void main() {
   final List<int> list1 = [1, 2, 4, 6];
   final List<int> list2 = [2, 3, 5];
 
@@ -10,4 +11,13 @@ void main() {
   final  matte = uniList..sort();
 
   print("The list united is: $matte");
+} */
+void main() {
+  final List<int> list1 = [1, 2, 4, 6];
+  final List<int> list2 = [2, 3, 5];
+
+  List<int> uniList = list1 + list2;
+  final matte = uniList.toSet().toList()..sort();
+
+  print("The list united and no repeated number is: $matte");
 }
