26533d53e05805b1034f972faf05f81e06ef14e9
bastndev <gohit24@gmail.com>
2024-01-09 11:06:33 -0500

    :arrow_up: Update `RECENT`

---
+++ 05-STACKS_QUEUEST/exercise2.dart
@@ -0,0 +1,53 @@
+class Stack {
+  List<int> _stack = [];
+
+  bool get isEmpty => _stack.isEmpty;
+
+  void push(int value) => _stack.add(value);
+
+  int pop() {
+    if (isEmpty) {
+      throw Exception('Stack is empty');
+    }
+    return _stack.removeLast();
+  }
+
+  int get top {
+    if (isEmpty) {
+      throw Exception('Stack is empty');
+    }
+    return _stack.last;
+  }
+}
+
+void sortStack(Stack stack) {
+  Stack tempStack = Stack();
+
+  while (!stack.isEmpty) {
+    int temp = stack.pop();
+
+    while (!tempStack.isEmpty && tempStack.top > temp) {
+      stack.push(tempStack.pop());
+    }
+
+    tempStack.push(temp);
+  }
+
+  while (!tempStack.isEmpty) {
+    stack.push(tempStack.pop());
+  }
+}
+
+void main() {
+  Stack stack = Stack();
+  stack.push(5);
+  stack.push(1);
+  stack.push(4);
+  stack.push(2);
+
+  sortStack(stack);
+
+  while (!stack.isEmpty) {
+    print(stack.pop());
+  }
+}
