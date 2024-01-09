a4feb922a870eb88a9383d11466f72a40892fb2d
bastndev <gohit24@gmail.com>
2024-01-09 09:33:02 -0500

    :arrow_up: Update `RECENT`

--- 05-STACKS_QUEUEST/queues.dart
+++ 05-STACKS_QUEUEST/queues.dart
@@ -1,6 +1,6 @@
-//-TODO: Cola
+//-TODO: Cola FIFO
 
-class ColaConDosPilas {
+/* class ColaConDosPilas {
   List _pilaEntrance = [];
   List _pilaExit = [];
 
@@ -30,13 +30,48 @@ void main() {
   miCola.enqueue(2);
   miCola.enqueue(3);
 
-  print("Remove Element of the Queues : ${miCola.dequeue()}");
-  print("Remove Element of the Queues : ${miCola.dequeue()}");
+  print("Remove Element of the Queues: ${miCola.dequeue()}");
+  print("Remove Element of the Queues: ${miCola.dequeue()}");
 
   miCola.enqueue(4);
   miCola.enqueue(5);
 
   while (!miCola.isEmpty()) {
-    print("Remove Element of the Queues : ${miCola.dequeue()}");
+    print("Remove Element of the Queues: ${miCola.dequeue()}");
+  }
+}
+ */
+
+class ColaBasic {
+  List _elements = [];
+
+  void enqueue(dynamic element) {
+    _elements.add(element);
+  }
+
+  dynamic dequeue() {
+    if (_elements.isNotEmpty) {
+      return _elements.removeAt(0);
+    } else {
+      return null; 
+    }
+  }
+
+  bool isEmpty() {
+    return _elements.isEmpty;
+  }
+}
+
+void main() {
+  ColaBasic miCola = ColaBasic();
+
+  miCola.enqueue('Maria');
+  miCola.enqueue('Juan');
+  miCola.enqueue('Lucas');
+
+  print("Elementos de la cola:");
+
+  while (!miCola.isEmpty()) {
+    print(miCola.dequeue());
   }
 }
