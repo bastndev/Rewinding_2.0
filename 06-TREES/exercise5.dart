f87a20cbf00413e7216724a6e0d01da8d7fa3009
bastndev <gohit24@gmail.com>
2024-01-10 16:18:01 -0500

    :alien: Update `new 2.0`

--- 06-TREES/exercise5.dart
+++ 06-TREES/exercise5.dart
@@ -1,1 +1,31 @@
+void main() {
+  var first = Node(4);
+  first.left = Node(5);
+  first.right = Node(7);
+  first.left?.left = Node(1);
+  first.left?.right = Node(3);
+  first.left?.left?.left = Node(8);
 
+  var second = Node(5);
+  second.left = Node(1);
+  second.right = Node(3);
+  second.left?.left = Node(8);
+
+  print("            ${first.val}");
+  print("       ${first.left!.val}        ${first.right!.val}");
+  print("   ${first.left?.left!.val}     ${first.left?.right!.val} ");
+  print("${first.left?.left?.left!.val}");
+
+  print("            ${second.val}");
+  print("       ${second.left!.val}        ${second.right!.val}");
+  print("   ${second.left?.left!.val} ");
+
+  print("====== SUB TREE ======");
+}
+
+class Node {
+  var val;
+  Node? left;
+  Node? right;
+  Node(this.val, {this.left, this.right});
+}
