027b87e1bde554c04661b4f11d7d03bb0c75866e
bastndev <gohit24@gmail.com>
2024-01-10 15:11:47 -0500

    :alien: Update `new 2.0`

---
+++ 06-TREES/exercise.3.dart
@@ -0,0 +1,22 @@
+void main() {
+  var root = Node(4);
+  root.left = Node(2);
+  root.right = Node(7);
+  root.left?.left = Node(1);
+  root.left?.right = Node(3);
+  root.left?.left?.left = Node(8);
+
+  print("            ${root.val}");
+  print("       ${root.left!.val}        ${root.right!.val}");
+  print("   ${root.left?.left!.val}     ${root.left?.right!.val} ");
+  print("${root.left?.left?.left!.val}");
+
+  print("====== DEPTH ======");
+}
+
+class Node {
+  var val;
+  Node? left;
+  Node? right;
+  Node(this.val, {this.left, this.right});
+}
