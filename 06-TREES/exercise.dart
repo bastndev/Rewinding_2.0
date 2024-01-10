f73d3a2cddb5dd2bec4150465a1ff478a84b48bf
bastndev <gohit24@gmail.com>
2024-01-10 11:14:59 -0500

    :alien: Update `new 2.0`

---
+++ 06-TREES/exercise.dart
@@ -0,0 +1,49 @@
+void main() {
+  var root = TreeNode(4);
+  root.left = TreeNode(2);
+  root.right = TreeNode(7);
+  root.left?.left = TreeNode(1);
+  root.left?.right = TreeNode(3);
+  root.right?.right = TreeNode(6);
+  root.right?.left = TreeNode(9);
+
+  print(root.val);
+  print(root.left!.val);
+  print(root.right!.val);
+  print(root.left?.left!.val);
+  print(root.left?.right!.val);
+  print(root.right?.right!.val);
+  print(root.right?.left!.val);
+
+  var inverseBinaryTree = InverseBinaryTree();
+  inverseBinaryTree.reverseTree(root);
+  print(root.val);
+  print(root.left!.val);
+  print(root.right!.val);
+  print(root.left?.left!.val);
+  print(root.left?.right!.val);
+  print(root.right?.right!.val);
+  print(root.right?.left!.val);
+}
+
+class TreeNode {
+  var val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode(this.val, {this.left, this.right});
+}
+
+class InverseBinaryTree {
+  void reverseTree(TreeNode? root) {
+    if (root == null) {
+      return;
+    }
+    TreeNode? tmp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(tmp);
+  }
+
+  TreeNode? invertTree(TreeNode? node) {
+    return node;
+  }
+}
