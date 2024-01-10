e69b5db5a5481bf46adccf0b3d8966ed36a86c17
bastndev <gohit24@gmail.com>
2024-01-10 15:02:48 -0500

    :alien: Update `new 2.0`

--- 06-TREES/practice2.0.dart
+++ 06-TREES/practice2.0.dart
@@ -1,3 +1,12 @@
-void main() { 
-  
+void main() {
+  final List<int> list = [2, 3, 4, 6, 7, 8, 9, 10];
+  print("====== LOOP ======");
+
+  for (var lista = 1; lista < list.length; lista++) { //This type of loop is used when you need to have more precise control
+    print("Hi: $lista");
+  }
+
+  for (var lista in list) { //This type of loop is used to iterate directly over the elements of a collection
+    print("Hello: $lista");
+  }
 }
