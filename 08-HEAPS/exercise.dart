ab68da4bb1816de5737f6b47ab4d671c23ebb7f4
bastndev <gohit24@gmail.com>
2024-01-12 12:02:23 -0500

    arrow_up Update `new 2.0`

---
+++ 08-HEAPS/exercise.dart
@@ -0,0 +1,32 @@
+import 'package:collection/collection.dart';
+
+class KthLargest {
+  int _k;
+  late PriorityQueue<int> heap;
+
+  KthLargest(int k, List<int> nums) {
+    this._k = k;
+    heap = PriorityQueue<int>((a, b) => a - b);
+
+    for (int num in nums) {
+      add(num);
+    }
+  }
+
+  int add(int val) {
+    heap.add(val);
+    while (heap.length > _k) {
+      heap.removeFirst();
+    }
+    return heap.first;
+  }
+}
+
+void main() {
+  KthLargest kthLargest = KthLargest(4, [1, 2, 3, 4, 5]);
+
+  print(kthLargest.add(6));
+  print(kthLargest.add(2));
+  print(kthLargest.add(1));
+  print(kthLargest.add(8));
+}
