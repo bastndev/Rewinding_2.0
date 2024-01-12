88dceb2ce452d4f94c1617502f46b918857f3ce1
bastndev <gohit24@gmail.com>
2024-01-12 11:22:31 -0500

    arrow_up Update `new 2.0`

--- 08-HEAPS/mein.dart
+++ 08-HEAPS/mein.dart
@@ -3,10 +3,13 @@ class MinHeap {
   int size;
   int maxSize;
 
-  MinHeap({required this.heap, required this.size, required this.maxSize});
+  MinHeap(int maxSize)
+      : this.heap = List<int>.filled(maxSize, 0, growable: false),
+        this.size = 0,
+        this.maxSize = maxSize;
 
   int _parentIndex(int i) {
-    return (i - 1) ~/ 1;
+    return (i - 1) ~/ 2;
   }
 
   int _leftChildIndex(int i) {
@@ -24,15 +27,82 @@ class MinHeap {
     return false;
   }
 
-  void insert(int element){
-    if(size >= maxSize) {
+  void insert(int element) {
+    if (size >= maxSize) {
       return;
     }
     heap[size] = element;
     int current = size;
 
-    while(heap[current] < heap[_parentIndex(current)]) {
-      
+    while (heap[current] < heap[_parentIndex(current)]) {
+      _swap(current, _parentIndex(current));
+      current = _parentIndex(current);
+    }
+    size++;
+  }
+
+  int extractMin() {
+    if (size <= 0) return double.infinity.toInt();
+
+    int popped = heap[0];
+    heap[0] = heap[--size];
+    _minHeapify(i: 0);
+    return popped;
+  }
+
+  void _minHeapify(int i) {
+    if (!_isLeaf(i)) {
+      if (heap[i] < heap[_leftChildIndex(i)] ||
+          heap[i] > heap[_rightChildIndex(i)]) {
+        if (heap[_leftChildIndex(i)] < heap[_rightChildIndex(i)]) {
+          _swap(i, _leftChildIndex(i));
+        } else {
+          _swap(i, _rightChildIndex(i));
+          _minHeapify(_rightChildIndex(i));
+        }
+      }
     }
   }
+
+  void printHeapPretty() {
+    for (int i = 0; i < (size ~/ 2); i++) {
+      print("Parent: ${heap[i]}");
+      if (_leftChildIndex(i) < size) print("Left: ${_leftChildIndex(i)}");
+      if (_rightChildIndex(i) < size) print("Left: ${_rightChildIndex(i)}");
+      print("");
+    }
+  }
+
+  void printHeap() {
+    print(heap.toString());
+  }
+
+  void _swap(int x, int y) {
+    int tmp;
+    tmp = heap[x];
+    heap[x] = heap[y];
+    heap[y] = tmp;
+  }
+}
+
+void main() {
+  MinHeap minHeap = MinHeap(20);
+  minHeap.insert(5);
+  minHeap.insert(10);
+  minHeap.insert(12);
+  minHeap.printHeap();
+  minHeap.insert(3);
+  minHeap.printHeap();
+  minHeap.insert(9);
+  minHeap.insert(15);
+  minHeap.insert(1);
+  minHeap.printHeap();
+
+  print(minHeap.extractMin());
+  print(minHeap.extractMin());
+  print(minHeap.extractMin());
+  print(minHeap.extractMin());
+  print(minHeap.extractMin());
+  print(minHeap.extractMin());
+  print(minHeap.extractMin());
 }
