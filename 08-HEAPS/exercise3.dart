b5589058ca593e49630a1c24cfdfce350fa72840
bastndev <gohit24@gmail.com>
2024-01-12 14:40:06 -0500

    arrow_up Update `new 2.0`

--- 08-HEAPS/exercise3.dart
+++ 08-HEAPS/exercise3.dart
@@ -1,8 +1,6 @@
 void main() {
-  final List<int> arrival = [5, 4, 3, 2, 1];
-  final List<String> medails = ['Gold', 'Silver', 'Bronze'];
+  final List<int> arrival = [10, 3, 8, 9, 4];
+  final List<String> medails = ['Gold Medal', 'Silver Medal', 'Bronze Medal'];
 }
 
-class YouMedals {
-  
-}
+class YouMedals {}
