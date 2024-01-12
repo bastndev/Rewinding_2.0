ac32da307bdcff3e740a6aec04fd3a8524005340
bastndev <gohit24@gmail.com>
2024-01-12 10:28:43 -0500

    arrow_up Update `new 2.0`

--- 08-HEAPS/mein.dart
+++ 08-HEAPS/mein.dart
@@ -2,6 +2,14 @@ class MinHeap {
   List<int> heap;
   int size;
   int maxSize;
-  
+
   MinHeap({required this.heap, required this.size, required this.maxSize});
+
+  int _parentIndex(int i) {
+    return (i - 1) ~/ 1;
+  }
+
+  int _leftChildIndex(int i) {
+    return (i * 2) + 1;
+  }
 }
