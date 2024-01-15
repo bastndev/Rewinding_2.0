bceafe6e67b70001e20586f5a09e0204cd82adce
bastndev <gohit24@gmail.com>
2024-01-15 10:29:54 -0500

    :arrow_up: Update `new 2.0`

--- 09-TRIES/exercise1.dart
+++ 09-TRIES/exercise1.dart
@@ -16,7 +16,7 @@ void main() {
 }
  */
 
-import 'dart:io';
+/* import 'dart:io';
 
 void main() {
   final List<String> books = ['Whatever', 'Book 1', 'water', 'Book 35'];
@@ -29,9 +29,31 @@ void main() {
         book.toLowerCase().startsWith(userInput.toLowerCase().substring(0, 2)));
 
     if (bookFound) {
-      print("Yea, I have your book");
+      print("Yea, I have ");
     } else {
       print("Sorry, my friend, we don't have your book");
+    }
+  }
+}
+ */
+
+import 'dart:io';
+
+void main() {
+  final List<String> books = ['Whatever', 'Book 1', 'water', 'Book 35'];
+
+  stdout.write('What book are you looking for?: ');
+  String userInput = stdin.readLineSync() ?? '';
+
+  if (userInput.isNotEmpty) {
+    String? foundBook = books.firstWhere(
+        (book) => book.toLowerCase().startsWith(userInput.toLowerCase()),
+        orElse: () => '');
+
+    if (foundBook.isNotEmpty) {
+      print("Yes!,We have: $foundBook");
+    } else {
+      print("sorry my friend, we don't have your book");
     }
   }
 }
