56f11aa97260c19a98f049db53eeddd5bed2903c
bastndev <gohit24@gmail.com>
2024-01-15 10:26:11 -0500

    :arrow_up: Update `new 2.0`

--- 09-TRIES/exercise1.dart
+++ 09-TRIES/exercise1.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+/* import 'dart:io';
 
 void main() {
   final List<String> books = ['Whatever', 'Book 1', 'water', 'Book 35'];
@@ -12,5 +12,26 @@ void main() {
     print("Yea I have your book");
   } else if (bookFound == false) {
     print("Sorry my friend we don't have your book");
+  }
+}
+ */
+
+import 'dart:io';
+
+void main() {
+  final List<String> books = ['Whatever', 'Book 1', 'water', 'Book 35'];
+
+  stdout.write('What book are you looking for?: ');
+  String userInput = stdin.readLineSync() ?? '';
+
+  if (userInput.isNotEmpty) {
+    bool bookFound = books.any((book) =>
+        book.toLowerCase().startsWith(userInput.toLowerCase().substring(0, 2)));
+
+    if (bookFound) {
+      print("Yea, I have your book");
+    } else {
+      print("Sorry, my friend, we don't have your book");
+    }
   }
 }
