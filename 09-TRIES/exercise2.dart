620649ddec8ff801a26d9252d9f1015906bfbe37
bastndev <gohit24@gmail.com>
2024-01-15 10:58:50 -0500

    :arrow_up: Update `new 2.0`

---
+++ 09-TRIES/exercise2.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<List<String>> board = [
+    ['p', 'e', 'r', 'o'],
+    ['a', 't', 'a', 'e'],
+    ['t', 'e', 'l', 'v'],
+    ['o', 'f', 'l', 'v']
+  ];
+
+  List<String> words = ['pero', 'pato', 'comida', 'veo', 'pata'];
+}
+
+class WordSearchSolution {
+  
+}
