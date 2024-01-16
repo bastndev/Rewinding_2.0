b9f0395a2550f438f1cb933fa14bde09d48d1197
bastndev <gohit24@gmail.com>
2024-01-16 15:26:11 -0500

    :arrow_up: Update `new 2.0`

--- 00-REWIND/one.dart
+++ 00-REWIND/one.dart
@@ -62,10 +62,36 @@
 }
  */
 
-void main() {
+/* void main() {
   bool valor1 = true;
   bool valor2 = false;
   bool valor3 = true;
   print(valor1 && valor2 && valor3);
   print(valor1 || valor2 || valor3);
+} */
+
+class Car {
+  String make;
+  String model;
+  String year;
+  bool hasABS;
+
+  // Constructor
+  Car(this.make, this.model, this.year, this.hasABS);
+
+  // Method to display car details
+  void displayDetails() {
+    print('Make: $make');
+    print('Model: $model');
+    print('Year: $year');
+    print('Has ABS?: ${hasABS ? 'Yes' : 'No'}');
+  }
+}
+
+void main() {
+  // Create an instance of Car
+  Car myCar = Car('Toyota', 'Corolla', '2020', true);
+
+  // Display car details
+  myCar.displayDetails();
 }
