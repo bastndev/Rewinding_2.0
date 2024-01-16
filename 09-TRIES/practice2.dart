731e929ea56dd44ca785ebb8f7aa052c3b1397c4
bastndev <gohit24@gmail.com>
2024-01-15 23:10:14 -0500

    :arrow_up: Update `new 2.0`

--- 09-TRIES/practice2.dart
+++ 09-TRIES/practice2.dart
@@ -1,4 +1,4 @@
-class Persona {
+/* class Persona {
   String name;
   int age;
 
@@ -12,4 +12,17 @@ class Persona {
 void main() {
   Persona persona = Persona('Gohit Bastian', 25);
   persona.showAge();
+}
+ */
+
+class Person {
+  final String name;
+  final int age;
+
+  Person(this.name, this.age);
+}
+
+void main() {
+  Person person = Person('Gohit', 25);
+  print("My name is ${person.name} I'm ${person.age} years old ");
 }
