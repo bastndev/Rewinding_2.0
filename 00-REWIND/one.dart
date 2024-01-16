8fed660118883079f8586d88580dc36e984f4564
bastndev <gohit24@gmail.com>
2024-01-16 14:49:41 -0500

    :arrow_up: Update `new 2.0`

---
+++ 00-REWIND/one.dart
@@ -0,0 +1,25 @@
+//-TODO: Boolean
+/* void main() {
+  bool active = true;
+  // print("This is Active: $active");
+
+  //Control structure
+
+  if (active == true) {
+    print("This is active");
+  } else {
+    print("Sorry is not active");
+  }
+} */
+
+void main() {
+  bool active = true;
+
+  active = !active;
+  //Control structure
+  if (active == true) {
+    print("This is active");
+  } else {
+    print("Sorry is not active");
+  }
+}
