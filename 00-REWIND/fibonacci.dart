669620c1b7d4e4576260d0eebea445afe89386bc
bastndev <gohit24@gmail.com>
2024-01-16 15:43:46 -0500

    :arrow_up: Update `new 2.0`

---
+++ 00-REWIND/fibonacci.dart
@@ -0,0 +1,32 @@
+/* int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+var result = fibonacci(20);
+void main() {
+  print(fibonacci(50));
+} */
+
+//NO fibonacci but us exercise of dart
+class Spacecraft {
+  String name;
+  DateTime? launchDate;
+
+  int? get launchYear => launchDate?.year;
+
+  Spacecraft(this.name, this.launchDate) {}
+
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  void describe() {
+    print('Spacecraft: $name');
+    var launchDate = this.launchDate;
+    if (launchDate != null) {
+      int years = DateTime.now().difference(launchDate).inDays ~/ 365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
