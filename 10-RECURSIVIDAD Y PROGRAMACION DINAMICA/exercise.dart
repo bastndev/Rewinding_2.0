2767e9e31e031bd9358dd3f868feee5d4413fc2b
bastndev <gohit24@gmail.com>
2024-01-16 11:28:12 -0500

    :arrow_up: Update `new 2.0`

--- 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/exercise.dart
+++ 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/exercise.dart
@@ -1,3 +1,54 @@
-void main() { 
-  
+class Cell {
+  int row, col;
+
+  Cell(this.row, this.col);
+}
+
+class RobotInGrid {
+  List<Cell> path = [];
+
+  List<Cell>? getPath(List<List<bool>> grid) {
+    if (_getPath(grid, 0, 0)) {
+      path = path.reversed.toList();
+      return path;
+    }
+    return null;
+  }
+
+  bool _getPath(List<List<bool>> grid, int row, int col) {
+    if (row >= grid.length || col >= grid[0].length || !grid[row][col]) {
+      return false;
+    }
+    bool isAtFinish = (row == grid.length - 1 && col == grid[0].length - 1);
+    if (isAtFinish ||
+        _getPath(grid, row, col + 1) ||
+        _getPath(grid, row + 1, col)) {
+      path.add(Cell(row, col));
+      return true;
+    }
+    grid[row][col] = false;
+    return false;
+  }
+}
+
+void main() {
+  RobotInGrid r = RobotInGrid();
+
+  List<List<bool>> grid = [
+    [true, true, true, true],
+    [false, false, false, true],
+    [true, true, false, true],
+    [true, true, false, true]
+  ];
+
+  List<Cell>? result = r.getPath(grid);
+
+  if (result != null) {
+    print("Camino encontrado:");
+    result.forEach((cell) {
+      print("(${cell.row}, ${cell.col})");
+    });
+  } else {
+    print("No hay camino posible.");
+  }
 }
