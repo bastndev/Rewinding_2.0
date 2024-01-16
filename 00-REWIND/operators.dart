c4e3da54a5701a0450ed586000a49db49be467e1
bastndev <gohit24@gmail.com>
2024-01-16 15:54:56 -0500

    :arrow_up: Update `new 2.0`

--- 00-REWIND/operators.dart
+++ 00-REWIND/operators.dart
@@ -8,4 +8,17 @@
   num = num % 5;
   print(num);
 } */
-//logical Operators
+
+//logical Operators == , != , <= , >=
+void main() {
+  int num = 30;
+  num = num + 22;
+  print(num);
+
+  num = num % 5;
+  if (num == 0) {
+    print('Cero');
+  } else {
+    print("Is not Cero is: $num");
+  }
+}
