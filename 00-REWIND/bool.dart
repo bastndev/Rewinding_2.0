6eb7de904839b8faf5448bc463ab447a21a2f176
bastndev <gohit24@gmail.com>
2024-01-16 15:34:25 -0500

    :arrow_up: Update `new 2.0`

--- 00-REWIND/bool.dart
+++ 00-REWIND/bool.dart
@@ -1,7 +1,19 @@
 class Student {
   final String name;
-  final num notes;
+  final num note;
   final bool approved;
 
-  Student(this.name, this.notes, this.approved);
+  Student(this.name, this.note, this.approved);
+
+  void dataNotes() {
+    print("Name: $name");
+    print("Notes: $note");
+    print("approved: ${approved ? 'Yes' : 'No'}");
+  }
+}
+
+void main() {
+  Student student = Student('Juan', 12, false);
+
+  student.dataNotes();
 }
