bb0bc92b376b43d4c9b549f9775192b091fff22e
bastndev <gohit24@gmail.com>
2024-01-16 14:52:03 -0500

    :arrow_up: Update `new 2.0`

--- 00-REWIND/one.dart
+++ 00-REWIND/one.dart
@@ -12,12 +12,25 @@
   }
 } */
 
+/* void main() {
+  bool active = true;
+
+  active = !active;
+  //Control structure || Condition structure
+  if (active == true) {
+    print("This is active");
+  } else {
+    print("Sorry is not active");
+  }
+}
+ */
+
 void main() {
   bool active = true;
 
   active = !active;
-  //Control structure
-  if (active == true) {
+  //equivalent
+  if (active) {
     print("This is active");
   } else {
     print("Sorry is not active");
