7847367e3cb0449d29944e2bf6ac7aba7c40f400
bastndev <gohit24@gmail.com>
2024-01-16 09:47:01 -0500

    :arrow_up: Update `new 2.0`

--- 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/main.dart
+++ 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/main.dart
@@ -1,8 +1,13 @@
-void main() {
+//-TODO: is delayed
+
+/* void main() {
   int fibonacci(int n) {
     if (n < 2) return n;
     return fibonacci(n - 1) + fibonacci(n - 2);
   }
 
-  print(fibonacci(10));
+  print(fibonacci(50));
 }
+ */
+
+// -TODO: is so fast
