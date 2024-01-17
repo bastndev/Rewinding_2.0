41165816b3c0e4321168745ab786c7c241395147
bastndev <gohit24@gmail.com>
2024-01-17 16:41:38 -0500

    :arrow_up: Update `new 2.0`

---
+++ 00-REWIND/tips.dart
@@ -0,0 +1,5 @@
+// -TODO: FOR : We know the number of repetitions
+
+// -TODO: WHILE: We know when we want to stop repeating,| example red pole
+
+// -TODO: DO WHILE: We always need to long in at least once 
