32a8c1e832e54c342ddec1022ce6d7960663880a
bastndev <gohit24@gmail.com>
2024-01-17 17:39:44 -0500

    :arrow_up: Update `new 2.0`

--- 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/exercise3.dart
+++ 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/exercise3.dart
@@ -1,8 +1,38 @@
 class GenerateParenthesis {
+  List<String> generateParenthesis(int n) {
+    if (n <= 0) return [];
+    List<String> result = [];
+    _generateParenthesis(n, n, StringBuffer(), result);
+    return result;
+  }
 
+  void _generateParenthesis(
+      int openLeft, int closeLeft, StringBuffer current, List<String> result) {
+    if (openLeft == 0 && closeLeft == 0) {
+      result.add(current.toString());
+      return;
+    }
+
+    if (openLeft > 0) {
+      current.write('(');
+      _generateParenthesis(openLeft - 1, closeLeft, current, result);
+      current.clear();
+    }
+    if (closeLeft > openLeft) {
+      current.write(')');
+      _generateParenthesis(openLeft, closeLeft - 1, current, result);
+      current.clear();
+    }
+  }
 }
 
 void main() {
-  
-}
+  GenerateParenthesis gp = GenerateParenthesis();
+  List<String> result = gp.generateParenthesis(3);
 
+  print(result);
+
+  // for (String str in result) {
+  //   print(str);
+  // }
+}
