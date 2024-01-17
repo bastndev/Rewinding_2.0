0ef8e6142ad0850c4a306712535cada1b62dee35
bastndev <gohit24@gmail.com>
2024-01-17 16:42:17 -0500

    :arrow_up: Update `new 2.0`

--- 00-REWIND/tips.dart
+++ 00-REWIND/tips.dart
@@ -1,5 +1,8 @@
+/* -FIXME: LOOPS */
 // -TODO: FOR : We know the number of repetitions
 
 // -TODO: WHILE: We know when we want to stop repeating,| example red pole
 
 // -TODO: DO WHILE: We always need to long in at least once 
+
+/* -FIXME:  */
