16f30501b521c4c0701d00e411302a258a76d35a
bastndev <gohit24@gmail.com>
2024-01-17 22:31:31 -0500

    :arrow_up: Update `new 2.0`

---
+++ 00-REWIND/function.dart
@@ -0,0 +1,39 @@
+/* void greet() {
+  print("Hello My friend");
+}
+
+void main() {
+  greet();
+}
+ */
+
+/* void car(String color) {
+  print("My car is of color $color");
+}
+
+void main() {
+  car('red');
+  car('Blue');
+}
+ */
+
+/* void greet(String name) {
+  print("Hello my name is $name");
+}
+
+void main() {
+  greet('Juan');
+  greet('Maria');
+  greet('Lucas');
+  greet('Sara');
+}
+ */
+
+void greet(String hello){
+  print("$hello How are you?");
+}
+void main() { 
+  greet('Hola');
+  greet('Hi');
+  greet('Buenos dias,');
+}
