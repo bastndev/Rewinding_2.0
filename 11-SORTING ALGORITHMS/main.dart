e8f573913c9d38de4af2849542945f0c2cc964ef
bastndev <gohit24@gmail.com>
2024-01-18 09:38:39 -0500

    :arrow_up: Update `new 2.0`

--- 11-SORTING ALGORITHMS/main.dart
+++ 11-SORTING ALGORITHMS/main.dart
@@ -2,7 +2,7 @@
 // sort by selection
 
 //-FIXME: Static
-class Counter {
+/* class Counter {
   static int count = 0;
 
   Counter() {
@@ -18,4 +18,15 @@ void main() {
   print(Counter.count);
 
   print("$c1  $c2"); //commend this
+} */
+
+
+class MyClass {
+  static void myStaticMethod() {
+    print("This is a static method.");
+  }
+}
+
+void main() {
+  MyClass.myStaticMethod(); // Llamada a un método estático sin instanciar la clase
 }
