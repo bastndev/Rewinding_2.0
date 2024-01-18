b0bda7cbb1cc7f0eb56d998fc1bc9b32bc55434b
bastndev <gohit24@gmail.com>
2024-01-17 21:35:55 -0500

    :arrow_up: Update `new 2.0`

--- 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/practice1.dart
+++ 10-RECURSIVIDAD Y PROGRAMACION DINAMICA/practice1.dart
@@ -1,7 +1,17 @@
-void greet() {
+/* void greet() {
   print("Hello My friend");
 }
 
 void main() {
   greet();
+}
+ */
+
+void car(String color) {
+  print("My car is of color $color");
+}
+
+void main() {
+  car('red');
+  car('Blue');
 }
