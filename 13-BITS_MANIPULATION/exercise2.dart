34f85536599e82855b6db668489b5312dc3c8895
bastndev <gohit24@gmail.com>
2024-01-19 09:30:04 -0500

    :arrow_up: Update `new 2.0`

--- 13-BITS_MANIPULATION/exercise2.dart
+++ 13-BITS_MANIPULATION/exercise2.dart
@@ -1,4 +1,4 @@
-class ReverseInt {
+/* class ReverseInt {
   int reverseInt(int n) {
     for (int i = 0; i < 16; i++) {
       int firstBit = _getBit(n, i);
@@ -19,3 +19,14 @@ class ReverseInt {
     return (n & clearBitMask) | (bit << i);
   }
 }
+
+void main() {
+  ReverseInt n = ReverseInt();
+
+  int input = 43261596;
+  print("Input: $input");
+
+  int result = n.reverseInt(input);
+  print("Reverse output: $result");
+}
+ */
