32ce6edc1078c964090d211d629fe7e97dbcbb7e
bastndev <gohit24@gmail.com>
2024-01-19 10:02:50 -0500

    :arrow_up: Update `new 2.0`

--- 13-BITS_MANIPULATION/exercise4.dart
+++ 13-BITS_MANIPULATION/exercise4.dart
@@ -1,5 +1,23 @@
 class SumIntegers {
+  int getSum(int a, int b) {
+    int result = 0; //Variable para almacenar el result
+    int carry = 0; //Variable para almacenar el carry
 
+    for (int i = 0; i < 32; i++) {
+      int firstBit = _getBit(a, i);
+      int secondBit = _getBit(b, i);
+
+      int sum = firstBit ^ secondBit ^ carry;
+      result += _updateBit(result, i, sum);
+
+      carry = (firstBit == 1 && secondBit == 1 ||
+              firstBit == 1 && carry == 1 ||
+              carry == 1 && secondBit == 1)
+          ? 1
+          : 0;
+    }
+    return result;
+  }
 
   int _getBit(int n, int i) {
     return (n >> i) & 1;
