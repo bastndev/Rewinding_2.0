ab065e51dc78342c24a8bd919766fa24d7746b46
bastndev <gohit24@gmail.com>
2024-01-23 11:10:40 -0500

    :arrow_up: Update `new 2.0`

--- lib/main.dart
+++ lib/main.dart
@@ -16,3 +16,9 @@ int fibonacci(int n) {
   return n < 2 ? n : (fibonacci(n - 1) + (fibonacci(n - 2)));
 }
  */
+
+void main(List<String> arguments) {
+  print("Hello World!");
+  print("Hello World!");
+  print("Hello World!");
+}
