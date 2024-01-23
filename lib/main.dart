91c80a3e1802cdb6b995ddee82e1d3ba87840754
bastndev <gohit24@gmail.com>
2024-01-23 10:50:43 -0500

    :arrow_up: Update `new 2.0`

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
-void main() {
+/* void main() {
   for (int i = 1; i < 5; i++) {
     Future.delayed(Duration(seconds: i), () {
       print("Hello $i");
     });
   }
 }
+ */
+
+/* void main() {
+  var i = 20;
+  print("fibonacci($i) = ${fibonacci(i)}");
+}
+
+int fibonacci(int n) {
+  return n < 2 ? n : (fibonacci(n - 1) + (fibonacci(n - 2)));
+}
+ */
