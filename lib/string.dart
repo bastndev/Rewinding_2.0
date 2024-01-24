e8c4c086a7047f6c8c49dd0cd6ba9fc73d28d059
bastndev <gohit24@gmail.com>
2024-01-24 10:20:10 -0500

    :arrow_up: Update `new 2.0`

---
+++ lib/string.dart
@@ -0,0 +1,16 @@
+// @command:dart.rerunLastDebugSession
+void main() {
+  String one = "one";
+  String two = "two";
+  String three = """
+  Lucas
+ssd
+  Pedro 
+
+  Maria'
+""";
+
+  print(three);
+  print(one);
+  print(two);
+}
