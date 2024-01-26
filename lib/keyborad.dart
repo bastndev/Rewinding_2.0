10526a8bbcfa2e373c718000bc58fb9293734e6c
bastndev <gohit24@gmail.com>
2024-01-26 10:57:21 -0500

    :arrow_up: Update `new 2.0`

---
+++ lib/keyborad.dart
@@ -0,0 +1,16 @@
+void main() {
+  int number1 = 10;
+  double number2 = 10;
+
+  var input = "name";
+  var input2 = 2.3;
+  input = "Maria";
+
+  bool result = true;
+
+  final result2 = "    number2";
+  // reult2 = "Maria"; no change
+
+  print("$number1 $number2 $input  $input2");
+  print("$result is different $result2");
+}
