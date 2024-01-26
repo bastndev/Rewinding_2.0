8b61a1f68c0765f6b49292c927845bbdf06145ff
bastndev <gohit24@gmail.com>
2024-01-26 11:05:00 -0500

    :arrow_up: Update `new 2.0`

--- lib/bool.dart
+++ lib/bool.dart
@@ -1,6 +1,14 @@
-void main() {
+/* void main() {
   bool isTrue;
   isTrue = 2 == (1 + 1);
 
   print(isTrue);
 }
+ */
+
+/* void main() { 
+  final isTrue = 'hello' == 'hello';
+  final isTrue2 = 'hello' != 2;
+
+  print(isTrue2);
+} */
