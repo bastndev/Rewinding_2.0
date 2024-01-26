1d3e121bd3639bd0f2e2c793557d4a01b8b73333
bastndev <gohit24@gmail.com>
2024-01-26 11:42:29 -0500

    :arrow_up: Update `new 2.0`

--- lib/01-COLLECTION/list.dart
+++ lib/01-COLLECTION/list.dart
@@ -1,4 +1,4 @@
-void main() {
+/* void main() {
   List list1 = [12, 23];
   List list2 = ['Juan', 'Maria'];
 
@@ -15,4 +15,19 @@ void main() {
 
   final result = listToList[1][1];
   print("Name with my friend is $result and she have ${list1[1]} years old");
+}
+ */
+
+void main() {
+  List mySecondList = ['lucas', 'Hernan'];
+
+  var fixedList = List.filled(4, '', growable: false);
+  fixedList[0] = 'Gohit';
+  fixedList[1] = mySecondList[0];
+  print(fixedList);
+
+  print(mySecondList.length);
+  mySecondList.add('Hello');
+  print(mySecondList);
+  print(mySecondList.length);
 }
