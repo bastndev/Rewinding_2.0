ae8e329c5577bbc38d6719b30f49b98edc62388b
bastndev <gohit24@gmail.com>
2024-01-26 11:24:08 -0500

    :arrow_up: Update `new 2.0`

---
+++ lib/01-COLLECTION/list.dart
@@ -0,0 +1,14 @@
+void main() {
+  List list1 = [12, 23];
+  List list2 = ['Juan', 'Maria'];
+
+  List listToList = [
+    [10, 24],
+    [list2],
+    [12, 546, 7]
+  ];
+
+  var myName = list2[0];
+
+  print("my name is $myName");
+}
